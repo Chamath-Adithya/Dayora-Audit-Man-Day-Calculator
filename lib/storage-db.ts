6e2b1f92fd16f5157359747e3ec663d8e18598f0
Chamath Adithya <achamath1@gmail.com>
2025-09-18 14:08:28 +0530

    fix by gemini result page

--- lib/storage-db.ts
+++ lib/storage-db.ts
@@ -1,4 +1,9 @@
 import { prisma } from './database'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set')
+}
+
 import { z } from 'zod'
 
 // Validation schemas
