cd686debfba4cff9b0d434b76f8d2e1c7a02e1e5
Chamath Adithya <achamath1@gmail.com>
2025-09-25 23:33:58 +0530

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { hashSync } from 'bcryptjs'
-import { defaultConfig } from '../lib/default-config'
+import { defaultConfig } from '../lib/default-config.js'
 
 const prisma = new PrismaClient()
 
